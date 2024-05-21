4ddbd0139e1f4bf7c32cd265b62792ca53135a2f
antara <antara.das.dev@gmail.com>
2024-05-21 14:33:07 +0530

    Remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ToDoModule } from './to-do/to-do.module';
 import { AuthModule } from './auth/auth.module';
-// import { ConfigModule } from '@nestjs/config';
 
 @Module({
   // imports: [AuthModule, UsersModule],
   imports: [
-    // ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
     UserModule,
